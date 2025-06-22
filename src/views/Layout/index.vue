f4670b2cef94ee806f77f10aee3d5f45c057bdb6
jiajj <jiasuperjia@163.com>
2025-06-22 16:04:58 +0800

    三栏buju

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -7,5 +7,30 @@ import LayoutFooter from "./components/LayoutFooter.vue";
   <LayoutNav></LayoutNav>
   <LayoutHeader></LayoutHeader>
   <RouterView />
+  <div class="main">
+    <div class="left">left</div>
+    <div class="right">right</div>
+  </div>
   <LayoutFooter></LayoutFooter>
 </template>
+<style>
+.main{
+  border: 1px solid red;
+  display: flex;
+  /* overflow: hidden; */
+  height: 200px;
+}
+.left{
+/* float: left; */
+width: 100px;
+/* height: 200px; */
+background-color: aqua;
+}
+.right{
+  /* margin-left: 100px; */
+  border: 1px solid golds;
+  flex: 1;
+  /* overflow: hidden; */
+  /* width: auto; */
+}
+</style>
